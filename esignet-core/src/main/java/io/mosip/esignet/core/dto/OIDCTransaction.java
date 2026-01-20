6a07acd33e8268165daf4868105968cad6807975
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-01-20 10:18:14 +0530

    Update OIDCTransaction.java
    Signed-off-by: Dharanidharan-S-04 <dean12345.dd@gmail.com>

--- esignet-core/src/main/java/io/mosip/esignet/core/dto/OIDCTransaction.java
+++ esignet-core/src/main/java/io/mosip/esignet/core/dto/OIDCTransaction.java
@@ -16,7 +16,7 @@ import java.util.Set;
 
 @Data
 public class OIDCTransaction implements Serializable {
-    private static final long serialVersionUID = 2283752863051489132L;
+    private static final long serialVersionUID = 1L;
 
     String transactionId;
 
