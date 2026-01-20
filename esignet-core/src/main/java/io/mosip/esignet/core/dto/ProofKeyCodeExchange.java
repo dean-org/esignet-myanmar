d9b28135179e3e53207246eb12f29125ef8bf2a2
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-01-20 11:38:44 +0530

    Update ProofKeyCodeExchange.java
    Signed-off-by: Dharanidharan-S-04 <dean12345.dd@gmail.com>

--- esignet-core/src/main/java/io/mosip/esignet/core/dto/ProofKeyCodeExchange.java
+++ esignet-core/src/main/java/io/mosip/esignet/core/dto/ProofKeyCodeExchange.java
@@ -19,6 +19,7 @@ import static io.mosip.esignet.core.constants.Constants.S256;
 @Getter
 @Setter
 public class ProofKeyCodeExchange implements Serializable {
+    private static final long serialVersionUID = -3499629669723453908L;
 
     private String codeChallenge;
     private String codeChallengeMethod;
